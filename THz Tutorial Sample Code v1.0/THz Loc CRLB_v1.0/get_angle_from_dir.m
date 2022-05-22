453f4795d92393c99b75df093fb9637047c12385
Chen Hui <hui.chen@chalmers.se>
2022-05-22 22:15:50 +0200

    Delete THz Tutorial Sample Code v1.0 directory

--- THz Tutorial Sample Code v1.0/THz Loc CRLB_v1.0/get_angle_from_dir.m
+++
@@ -1,5 +0,0 @@
-% convert direction to angle
-function [phi, theta] = get_angle_from_dir(tv)
-    phi = atan2d(tv(2,:), tv(1,:));
-    theta = asind(tv(3,:));
-end
--- THz Tutorial Sample Code v1.0/THz Loc CRLB_v1.0/get_dir_from_angle.m
+++
@@ -1,6 +0,0 @@
-% convert angle to direction vector
-function tv = get_dir_from_angle(Phi)
-    phi = Phi(1);
-    theta = Phi(2);
-    tv = [cosd(theta)*cosd(phi), cosd(theta)*sind(phi), sind(theta)]';
-end
